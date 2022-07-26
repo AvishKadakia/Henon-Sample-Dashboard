958e041ba95cfc5a8b650e2afbd097954f0c6f2c
AvishKadakia <avishkadakia1996@gmail.com>
2022-07-25 23:03:25 -0400

    Testing

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -33,7 +33,7 @@ export const ContextProvider = ({ children }) => {
 
   const setLogin = (token, type) => {
     console.log(token);
-    if (type == "google") {
+    if (type == "google" && token !== "null") {
       const data = jwt_decode(token);
       setProfileInfo({
         name:
